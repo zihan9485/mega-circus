c6bd4a5186111d742487b6881bd8a7fa0ffa8c95
Akshay Narayan @ HP360 <okkhoy@gmail.com>
2021-02-12 14:10:07 +0800

    update animals with name field

--- src/main/java/circus/Trainer.java
+++ src/main/java/circus/Trainer.java
@@ -7,7 +7,7 @@ import circus.animal.Parrot;
 
 public class Trainer {
     public static void main(String[] args) {
-        Duck d = new Duck();
+        Duck d = new Duck("Donald");
         getToSpeak(d);
 
         Bird b = (Bird) d;  // upcasting
@@ -19,8 +19,8 @@ public class Trainer {
         Duck d2 = (Duck) a; // downcasting
         getToSpeak(d2);
 
-        train(new Duck());
-        train(new Parrot());
+        train(new Duck("Daisy"));
+        train(new Parrot("Chirpy"));
     }
 
     private static void getToSpeak(Animal animal) {
